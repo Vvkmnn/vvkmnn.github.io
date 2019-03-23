279d06fba7fda3e13f52cf9a4c05eb039702aaa2
Vivek Menon <mail@vvkmnn.xyz>
2019-03-23 18:12:47 -0400

    FIX(css): Minimize and cleanup

--- js/gradient.js
+++ js/gradient.js
@@ -2,38 +2,36 @@
 
 // https://apnyc.com/wp-content/uploads/2017/04/ColorChart_for_Web.jpg
 var colors = new Array(
-[244, 207, 41], // Saffron
-[230, 25, 75], //  Red      
-[0, 128, 128], //  Teal	 
-[230, 190, 255], //Lavender 
-[170, 255, 195], //Mint	 
-[145, 30, 180], // Purple	 
-[70, 240, 240], // Cyan	 
-[42, 201, 185], // Old Brick
-[173, 48, 41], // Tall Poppy
-[245, 130, 48], // Orange	 
-[255, 225, 25], // Yellow	 
-[229, 114, 94], // Terracotta
-[60, 180, 75], //  Green	 
-[70, 240, 240], // Cyan	 
-[0, 128, 128], //  Teal	 
-[230, 190, 255], //Lavender 
-[170, 255, 195], //Mint	 
-[255, 215, 180], //Coral	 
-[255, 225, 25], // Yellow	 
-[229, 114, 94], // Terracotta
-[250, 190, 190], //Pink	 
-[219, 187, 48], // Anzac
-[42, 201, 185], // Old Brick
-[128, 128, 128], //Grey	 
-[229, 114, 94], // Terracotta
-[250, 190, 190], //Pink	 
-[219, 187, 48], // Anzac
-[42, 201, 185], // Old Brick
-[173, 48, 41], // Tall Poppy
-[147, 20, 52] // Claret
-[250, 190, 190], //Pink	 
-[219, 187, 48], // Anzac
+  [244, 207, 41], // Saffron
+  [230, 25, 75], //  Red
+  [0, 128, 128], //  Teal
+  [230, 190, 255], //Lavender
+  [170, 255, 195], //Mint
+  [145, 30, 180], // Purple
+  [70, 240, 240], // Cyan
+  [42, 201, 185], // Old Brick
+  [173, 48, 41], // Tall Poppy
+  [245, 130, 48], // Orange
+  [255, 225, 25], // Yellow
+  [229, 114, 94], // Terracotta
+  [60, 180, 75], //  Green
+  [70, 240, 240], // Cyan
+  [0, 128, 128], //  Teal
+  [230, 190, 255], //Lavender
+  [170, 255, 195], //Mint
+  [255, 215, 180], //Coral
+  [255, 225, 25], // Yellow
+  [229, 114, 94], // Terracotta
+  [250, 190, 190], //Pink
+  [219, 187, 48], // Anzac
+  [42, 201, 185], // Old Brick
+  [128, 128, 128], //Grey
+  [229, 114, 94], // Terracotta
+  [250, 190, 190], //Pink
+  [219, 187, 48], // Anzac
+  [42, 201, 185], // Old Brick
+  [173, 48, 41], // Tall Poppy
+  [219, 187, 48] // Anzac
 );
 
 var step = 0;
@@ -49,43 +47,53 @@ var colorIndices = [0, 1, 2, 3];
 var gradientSpeed = 0.007;
 
 function updateGradient() {
+  if ($ === undefined) return;
 
-	if ($ === undefined) return;
+  var c0_0 = colors[colorIndices[0]];
+  var c0_1 = colors[colorIndices[1]];
+  var c1_0 = colors[colorIndices[2]];
+  var c1_1 = colors[colorIndices[3]];
 
-	var c0_0 = colors[colorIndices[0]];
-	var c0_1 = colors[colorIndices[1]];
-	var c1_0 = colors[colorIndices[2]];
-	var c1_1 = colors[colorIndices[3]];
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
 
-	var istep = 1 - step;
-	var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-	var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-	var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-	var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
 
-	var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-	var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-	var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+  $("#gradient-js")
+    .css({
+      background:
+        "-webkit-gradient(linear, left top, right top, from(" +
+        color1 +
+        "), to(" +
+        color2 +
+        "))"
+    })
+    .css({
+      background:
+        "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
+    });
 
-        $('#gradient-js').css({
-		background: "-webkit-gradient(linear, left top, right top, from(" + color1 + "), to(" + color2 + "))"
-	}).css({
-		background: "-moz-linear-gradient(left, " + color1 + " 0%, " + color2 + " 100%)"
-	});
+  step += gradientSpeed;
+  if (step >= 1) {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
 
-	step += gradientSpeed;
-	if (step >= 1) {
-		step %= 1;
-		colorIndices[0] = colorIndices[1];
-		colorIndices[2] = colorIndices[3];
-
-		//pick two new target color indices
-		//do not pick the same as the current one
-		colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-		colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
-
-	}
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] =
+      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+    colorIndices[3] =
+      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+  }
 }
 
 // console.log("gradient loaded!")
