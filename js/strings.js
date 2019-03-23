dd57698bd3b97a135b7a4a73529d654a26f68b8a
Vivek Menon <mail@vvkmnn.xyz>
2019-03-23 18:20:40 -0400

    FIX(typed): Hide about section

--- js/strings.js
+++ js/strings.js
@@ -1,28 +1,28 @@
 // strings
 
-$(function () {
-	console.log('type loaded!');
-	$("#definition").typed({
-		stringsElement: $('#definitions'),
-		// typing speed
-		typeSpeed: 8,
-		// time before typing starts
-		startDelay: 0,
-		// preserve characters
-		smartBackspace: true,
-		// backspacing speed
-		backSpeed: 15,
-		// shuffle the strings
-		shuffle: true,
-		// time before backspacing
-		backDelay: 1150,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: false,
-		// show cursor
-		showCursor: true,
-		// character for cursor
-		cursorChar: "_"
-	});
-})
+$(function() {
+  console.log("type loaded!");
+  $("#definition").typed({
+    stringsElement: $("#definitions"),
+    // typing speed
+    typeSpeed: 10,
+    // time before typing starts
+    startDelay: 0,
+    // preserve characters
+    smartBackspace: true,
+    // backspacing speed
+    backSpeed: 15,
+    // shuffle the strings
+    shuffle: true,
+    // time before backspacing
+    backDelay: 1150,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: false,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "_"
+  });
+});
